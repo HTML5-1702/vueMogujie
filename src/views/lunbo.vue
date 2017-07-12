fcd6025aec5f27cacb1b31da7435c09b4f0de252
hujian <1058839534@qq.com>
2017-07-12 21:18:14 +0800

    hj

--- src/views/lunbo.vue
+++ src/views/lunbo.vue
@@ -16,28 +16,20 @@
 	export default{
 		name:'lunbo',
 		props:{
-			message:[]
+			message:{}
 		},
         data() {  
             return {  
                 swiperOption: {  
                 //是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true  
-                notNextTick: true, 
-                autoplay: 2000, 
+                autoplay: 3000, 
                 loop:true,
-                effect:"coverflow",
-                pagination: '.swiper-pagination',  
-                slidesPerView: 'auto',  
+                pagination: '.swiper-pagination', 
+                mousewheelControl:true,
                 centeredSlides: true,  
                 paginationClickable: true, 
                 observeParents:true,
-                spaceBetween: 30,  
-                    onSlideChangeEnd: swiper => {  
-                        //这个位置放swiper的回调方法  
-                        this.page = swiper.realIndex+1;  
-                        this.index = swiper.realIndex;  
-                    }  
-                }  
+                }
             }  
         },
         components: {  
@@ -55,4 +47,7 @@
 
 </script>
 <style>
+	.swiper-pagination{text-align: left;margin: 0 0 0 0.1rem;}
+	.swiper-pagination-bullet{background: #FFFFFF;opacity: 1;}
+	.swiper-pagination-bullet-active{background: red;}
 </style>
