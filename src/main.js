3b81ce5c767f25e3e98a26ae84a59d9005d50cd3
haoran <477731280@qq.com>
2017-07-13 21:14:43 +0800

    haoran-7.13

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import Mine from './components/mine'
 import Login from './components/login'
 import Search from './components/search'
 import SearchList from './components/searchlist'
+import CategoryList from './components/categorylist'
 
 
 Vue.config.productionTip = false;
@@ -31,7 +32,8 @@ const routes = [
 	{path : '/mine' , component : Mine},
   {path:'/login',component:Login},
 	{path : '/search' , component : Search},
-	{path : '/searchlist' , component : SearchList}
+	{path : '/searchlist' , component : SearchList},
+	{path : '/categorylist' , component : CategoryList}
 ];
 
 //创建路由对象
